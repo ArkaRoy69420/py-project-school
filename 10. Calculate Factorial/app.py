23c7cbc61034d523f3980c4c5185d0ef3155d437
ArkaRoy69420 <137039462+ArkaRoy69420@users.noreply.github.com>
2025-02-16 23:07:38 +0530

    removal of math module

--- 10. Calculate Factorial/app.py
+++ 10. Calculate Factorial/app.py
@@ -5,4 +5,17 @@ print("\033[33mThis program finds factorial of a given number.\033[0m")
 
 NUMBER = int(input("Enter a number whose factorial you want to find: "))
 
-print(f"Factorial of {NUMBER} is {f(NUMBER)}")
+# example: factorial 5 is 1 x 2 x 3 x 4 x 5 = 120
+
+numbersArray = []
+res=1
+
+# get the numbers to be multiplied into an array
+for i in range(1, NUMBER+1):
+  numbersArray.append(i)
+
+# multiply the numbers and save it into "res" variable
+for i in numbersArray:
+  res = res *i
+
+print(f"Factorial of {NUMBER} is {res}")
